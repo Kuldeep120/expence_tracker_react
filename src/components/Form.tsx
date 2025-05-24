38ae11797ce8d9c7e7be04016c96b39f73fb00a3
Kuldeep Kumar <kuldeepkhatri03@gmail.com>
2025-05-24 18:03:15 +0500

    added form valdiation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,25 @@
 import { useRef, useState, type FormEvent } from "react";
 import { useForm, type FieldValues } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  
+  console.log(errors);
   const onSubmit = (data: FieldValues) => {
-    console.log(data)
-  }
+    console.log(data);
+  };
   // const handleSubmittV2 = (event: FormEvent, onSubmit)=>{
   //   console.log(event);
-  //   return onSubmit 
+  //   return onSubmit
   // }
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -17,18 +28,24 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3, maxLength: 20 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" ? (
+          <p>Name field is required</p>
+        ) : null}
+        {errors.name?.type === "minLength" ? (
+          <p>Name field should atleat be 3</p>
+        ) : null}
       </div>
       <div className="mb-5">
         <label htmlFor="age" className="form-label">
           Age
         </label>
         <input
-          {...register('age')}
+          {...register("age")}
           id="age"
           type="number"
           className="form-control"
